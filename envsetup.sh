561ccfbfe968a09f41418d4fe7c511bf2f76b78a
Sergey Nikolaienkov <sergeynv@google.com>
2021-12-05 15:40:16 +0100

    Add envsetup.sh

---
+++ envsetup.sh
@@ -0,0 +1,5 @@
+alias idea="/opt/intellij-ce-stable/bin/idea.sh"
+
+alias base="idea /usr/local/google/home/sergeynv/Desktop/AIDE/master/core+services"
+alias cdm="idea /usr/local/google/home/sergeynv/Desktop/AIDE/master/cdm"
+alias cts="idea /usr/local/google/home/sergeynv/Desktop/AIDE/master/cts"
