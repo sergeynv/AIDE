6b6b7c5bdad10989eafb54595635913e5b25ee14
Sergey Nikolaienkov <sergeynv@google.com>
2021-12-10 13:05:50 +0100

    Add ast alias and settings.jar

--- envsetup.sh
+++ envsetup.sh
@@ -1,4 +1,6 @@
 alias idea="/opt/intellij-ce-stable/bin/idea.sh"
+alias ast="/opt/android-studio-with-blaze-stable/bin/studio.sh"
+
 
 alias base="idea /usr/local/google/home/sergeynv/Desktop/AIDE/master/core+services"
 alias cdm="idea /usr/local/google/home/sergeynv/Desktop/AIDE/master/cdm"
