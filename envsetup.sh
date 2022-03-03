cfb38254bbf84f4f8a926c9ed4a2db7a801af62a
Sergey Nikolaienkov <sergeynv@google.com>
2022-03-03 14:19:42 +0100

    Add cconfig and paths aliases

--- envsetup.sh
+++ envsetup.sh
@@ -1,5 +1,7 @@
 __AIDE_HOME="/usr/local/google/home/sergeynv/Desktop/AIDE"
 alias croot="cd ${__AIDE_HOME}"
+alias cconfig="cd ~/.config/JetBrains/"
+alias paths="subl ~/.config/JetBrains/IdeaIC2021.2/options/path.macros.xml"
 
 alias idea="/opt/intellij-ce-stable/bin/idea.sh"
 alias ast="/opt/android-studio-with-blaze-stable/bin/studio.sh"
