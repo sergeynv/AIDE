67cb3061766e0766cfc5d31b23b2563a9a6fc5c7
Sergey Nikolaienkov <sergeynv@google.com>
2022-03-03 12:21:29 +0100

    Add __AIDE_HOME bash variable

--- envsetup.sh
+++ envsetup.sh
@@ -1,7 +1,9 @@
+__AIDE_HOME="/usr/local/google/home/sergeynv/Desktop/AIDE"
+alias croot="cd ${__AIDE_HOME}"
+
 alias idea="/opt/intellij-ce-stable/bin/idea.sh"
 alias ast="/opt/android-studio-with-blaze-stable/bin/studio.sh"
 
-
-alias base="idea /usr/local/google/home/sergeynv/Desktop/AIDE/master/core+services"
-alias cdm="idea /usr/local/google/home/sergeynv/Desktop/AIDE/master/cdm"
-alias cts="idea /usr/local/google/home/sergeynv/Desktop/AIDE/master/cts"
+alias base="idea ${__AIDE_HOME}/master/core+services"
+alias cdm="idea ${__AIDE_HOME}/master/cdm"
+alias cts="idea ${__AIDE_HOME}/master/cts"
