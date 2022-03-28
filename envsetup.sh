1c241a908972dc2fda5d633af0f2f9d34ca097da
Sergey Nikolaienkov <sergeynv@google.com>
2022-03-28 09:17:21 +0200

    Tweak envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -1,4 +1,5 @@
 __AIDE_HOME="/usr/local/google/home/sergeynv/Desktop/AIDE"
+
 alias croot="cd ${__AIDE_HOME}"
 alias cconfig="cd ~/.config/JetBrains/"
 alias paths="subl ~/.config/JetBrains/IdeaIC2021.2/options/path.macros.xml"
