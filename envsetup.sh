52bf2a211c0b06e64bc8b06c6e3909122fe10521
Sergey Nikolaienkov <sergeynv@google.com>
2022-05-09 09:39:57 +0200

    Tweak envsetup.sh
    Add explicit versioning.

--- envsetup.sh
+++ envsetup.sh
@@ -1,16 +1,28 @@
 __AIDE_HOME="/usr/local/google/home/sergeynv/Desktop/AIDE"
 
-__CONFIG_JETBRAINS="/usr/local/google/home/sergeynv/.config/JetBrains"
+# /opt/intellij-ce-2021.2
+__INTELLIJ_CE_VERSION="2021.2"
+# /opt/android-studio-with-blaze-2020.3
+__ANDROID_STUDIO_WITH_BLAZE_VERSION="2020.3"
 
-__IDEA_VERSION="IdeaIC2021.3"
-__CONFIG_IDEA="${__CONFIG_JETBRAINS}/${__IDEA_VERSION}"
+__JETBRAINS_CONFIGS="/usr/local/google/home/sergeynv/.config/JetBrains"
+__INTELLIJ_IDEA_CONFIGS="${__JETBRAINS_CONFIGS}/IdeaIC${__INTELLIJ_CE_VERSION}"
+
+
 
 alias croot="cd ${__AIDE_HOME}"
-alias cconfig="cd ${__CONFIG_JETBRAINS}"
-alias paths="subl ${__CONFIG_IDEA}/options/path.macros.xml"
+alias cconfig="cd ${__JETBRAINS_CONFIGS}"
+alias paths="subl ${__INTELLIJ_IDEA_CONFIGS}/options/path.macros.xml"
 
-alias idea="/opt/intellij-ce-stable/bin/idea.sh"
-alias ast="/opt/android-studio-with-blaze-stable/bin/studio.sh"
+
+
+alias idea="/opt/intellij-ce-${__INTELLIJ_CE_VERSION}/bin/idea.sh"
+alias idea-stable="/opt/intellij-ce-stable/bin/idea.sh"
+
+alias ast="/opt/android-studio-with-blaze-${__ANDROID_STUDIO_WITH_BLAZE_VERSION}/bin/studio.sh"
+alias ast-stable="/opt/android-studio-with-blaze-stable/bin/studio.sh"
+
+
 
 alias base="idea ${__AIDE_HOME}/master/core+services"
 alias cdm="idea ${__AIDE_HOME}/master/cdm"
