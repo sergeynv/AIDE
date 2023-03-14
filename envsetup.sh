5a0be776aaac1b595888fbc070807560939195e3
Sergey Nikolaienkov <sergeynv@google.com>
2023-03-14 11:02:45 +0100

    Tweak aliases in envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -16,14 +16,13 @@ __INTELLIJ_IDEA_CONFIGS="${__JETBRAINS_CONFIGS}/IdeaIC${__INTELLIJ_IDEA_VERSION}
 alias idea="${__INTELLIJ_IDEA_PATH}/bin/idea.sh"
 alias idea-stable="/opt/intellij-ce-stable/bin/idea.sh"
 
+alias idea-kill="killall ${__INTELLIJ_IDEA_PATH}/jbr/bin/java"
+
 alias idea-version="echo ${__INTELLIJ_IDEA_VERSION}"
-alias idea-bins="ls -ld /opt/intellij-ce-*"
+alias idea-ls-installed="ls -ld /opt/intellij-ce-*"
 
-alias idea-configs="cd ${__INTELLIJ_IDEA_CONFIGS}"
-alias idea-paths="subl ${__INTELLIJ_IDEA_CONFIGS}/options/path.macros.xml"
-
-alias kill-idea="killall ${__INTELLIJ_IDEA_PATH}/jbr/bin/java"
-
+alias idea-cd-configs-dir="cd ${__INTELLIJ_IDEA_CONFIGS}"
+alias idea-open-paths-config="open ${__INTELLIJ_IDEA_CONFIGS}/options/path.macros.xml"
 
 # Android Studio with Blaze
 __ASwB_VERSION="2020.3" # /opt/android-studio-with-blaze-2020.3
@@ -32,16 +31,15 @@ alias aswb="/opt/android-studio-with-blaze-${__ASwB_VERSION}/bin/studio.sh"
 alias aswb-stable="/opt/android-studio-with-blaze-stable/bin/studio.sh"
 
 alias aswb-version="echo ${__ASwB_VERSION}"
-alias aswb-bins="ls -ld /opt/android-studio-with-blaze-*"
+alias aswb-ls-installed="ls -ld /opt/android-studio-with-blaze-*"
 
-alias aswb-configs="echo '\"aswb-configs\" alias not set up'"
-alias aswb-paths="echo '\"aswb-paths\" alias not set up'"
-
+alias idea-cd-configs-dir="echo '\"aswb-configs\" alias not set up'"
+alias aswb-open-paths-config="echo '\"aswb-paths\" alias not set up'"
 
 # AIDE
 __AIDE_HOME="${HOME}/Desktop/AIDE"
 
-alias croot="cd ${__AIDE_HOME}"
+alias cdaide="cd ${__AIDE_HOME}"
 
 alias fw-base="idea ${__AIDE_HOME}/intellij-projects/frameworks-base"
 alias cts="idea ${__AIDE_HOME}/intellij-projects/cts"
